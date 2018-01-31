9ad42fc677e54990e6db54d12beb46ce9a724955
Kyle Morton <kylemortondevelopment@gmail.com>
2018-01-30 23:25:14 -0600

    created gulp tasks for js, css, linting, added react modules for use later

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,25 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect'); //run local dev server
 var open = require('gulp-open'); //open url in web browser
+var browserify = require('browserify'); //bundle js
+var reactify = require('reactify'); //transform JSX to JS
+var source = require('vinyl-source-stream'); //use conventional text streams with gulp
+var concat = require('gulp-concat'); //concat files
+var lint = require('gulp-eslint'); //lint js files, including jsx!
+
 
 var config = {
     port: 9005,
     devBaseUrl: 'http://localhost',
     paths: {
         html: './src/*.html', //in src directory, any file that has html extension
-        dist: './dist'
-
+        js: './src/**/*.js',
+        css: [
+            'node_modules/bootstrap/dist/css/bootstrap.min.css',
+            'node_modules/bootstrap/dist/css/bootstrap-theme.min.css',
+        ],
+        dist: './dist',
+        mainJs: './src/main.js'
     }
 }
 
@@ -38,9 +49,33 @@ gulp.task('html', function() {
         .pipe(connect.reload());
 });
 
+gulp.task('js', function() {
+    browserify(config.paths.mainJs)
+        .transform(reactify)
+        .bundle()
+        .on('error', console.error.bind(console)) //error show up in console
+        .pipe(source('bundle.js')) //name of bundle
+        .pipe(gulp.dest(config.paths.dist + '/scripts'))
+        .pipe(connect.reload());
+});
+
+gulp.task('css', function() {
+    gulp.src(config.paths.css)
+        .pipe(concat('bundle.css')) //bundle all css
+        .pipe(gulp.dest(config.paths.dist + '/css'));
+});
+
+gulp.task('lint', function() {
+    return gulp.src(config.paths.js)
+        .pipe(lint({config: 'eslint.config.json'}))
+        .pipe(lint.format());
+});
+
 //watches for changes to files in given directory, run task if changed
 gulp.task('watch', function() {
     gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.js, ['js', 'lint']);
+    gulp.watch(config.paths.css, ['css']);
 });
 
-gulp.task('default', ['html', 'open', 'watch']); //run 'gulp' to run html and open tasks
+gulp.task('default', ['html', 'js', 'css', 'lint', 'open', 'watch']); //run 'gulp' to run html and open tasks
---
+++ src/main.js
@@ -0,0 +1,5 @@
+$ = jQuery = require('jquery'); //access jquery thru '$' or 'jquery' word
+
+var App = console.log('hello world from Browserify');
+
+module.exports = App; //browserify looks for this to export into the bundled JS
